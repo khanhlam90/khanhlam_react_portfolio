d591e91f7baca67eef0e3a0d97551907abd93716
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-03-03 17:31:41 -0800

    About component done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import About from './components/About';
-import herobg from './assets/cover/hero-bg.jpg';
 
 function App() {
 
   return (
     <div>
-      <img 
-        src={herobg} 
-        className="my-2" 
-        style={{ width: "100%"}} 
-        alt="hero" 
-      />
       <main>
       <About></About>
       </main>
